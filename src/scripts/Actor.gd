3d6199412578f68f9c1bb91bc4aa1f2d0f4c8880
Lucas Santana <lanatnas36@gmail.com>
2020-09-26 00:20:58 -0300

    all files

---
+++ src/scripts/Actor.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP 
+
+export var speed: = Vector2(800.0, 1440.0)
+export var gravity: = 4000.0
+
+var velocity: = Vector2.ZERO
+
+
+
